ab563cf66f095c9bdf155bb6d0275636a51ea5be
Gabriel Oriente <gabriel.oriente@hotmail.com>
2021-06-10 21:37:30 -0300

    Estruturadecontrole_V4

---
+++ Estrutura de Controle/desafio_set.py
@@ -0,0 +1,12 @@
+PALAVRAS_PROIBIDAS = {'futebol', 'religião', 'política'}
+textos = [
+    'João gosta de futebol e política',
+    'A praia foi divertida',
+]
+
+for texto in textos:
+    intersecao = PALAVRAS_PROIBIDAS.intersection(set(texto.lower().split()))
+    if intersecao:
+        print('Texto possui palavra(s) proibida(s): ', intersecao)
+    else:
+        print('Texto autorizado:', texto)
--- Estrutura de Controle/for_4.py
+++ Estrutura de Controle/for_4.py
@@ -11,3 +11,20 @@
 # se o numero for par e for igual ao valor sorteado pela funcao dado, imprimir `Acertou`
 # e depois chamar o break
 # se nao acertar chama o else... print('Nao acertou')
+
+from random import randint
+
+
+def sortear_dado():
+    return randint(1, 6)
+
+
+for i in range(1, 7):
+    if i % 2 == 1:
+        continue
+
+    if sortear_dado() == i:
+        print('Acertou', i)
+        break
+else:
+    print('ERROU')
