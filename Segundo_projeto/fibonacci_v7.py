26cbeb3977354f2c6353131cf300edfae93ee6ea
Gabriel Oriente <gabriel.oriente@hotmail.com>
2021-06-14 19:43:02 -0300

    Estrutura

---
+++ Segundo_projeto/fibonacci_v7.py
@@ -0,0 +1,12 @@
+def fibonacci(quantidade):
+    resultado = [0, 1]
+
+    for i in range(2, quantidade):
+        resultado.append(sum(resultado[-2:]))
+    return resultado
+
+
+if __name__ == '__main__':
+    # listar os 20 primeiros números da sequência
+    for fib in fibonacci(20):
+        print(fib)
