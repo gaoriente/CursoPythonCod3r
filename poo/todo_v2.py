50ef1981c2ddd9aacbc590db06b7d53b9001dc4f
Gabriel Oriente <gabriel.oriente@hotmail.com>
2021-06-26 19:19:44 -0300

    OrientacaoObjeto_v4

--- poo/todo_v2.py
+++ poo/todo_v2.py
@@ -1,6 +1,26 @@
 from datetime import datetime
 
 
+class Projeto:
+    def __init__(self, nome):
+        self.nome = nome
+        self.tarefas = []
+
+    def add(self, descricao):
+        self.tarefas.append(Tarefa(descricao))
+
+    def pendentes(self):
+        return [tarefa for tarefa in self.tarefas if not tarefa.feito]
+
+    def procurar(self, descricao):
+        # Possível indexError, precisamos tratar
+        return [tarefa for tarefa in self.tarefas
+                if tarefa.descricao == descricao[0]]
+
+    def __str__(self):
+        return f'{self.nome} ({len(self.pendentes())} tarefa(s) pendente(s))'
+
+
 class Tarefa:
     def __init__(self, descricao):
         self.descricao = descricao
@@ -15,12 +35,24 @@ class Tarefa:
 
 
 def main():
-    casa = []
-    casa.append(Tarefa('Passar roupa'))
-    casa.append(Tarefa('Lavar prato'))
+    casa = Projeto('Tarefas de casa')
+    casa.add('Passar roupa')
+    casa.add('Lavar prato')
+    print(casa)
 
-    [tarefa.concluir() for tarefa in casa if tarefa.descricao == 'Lavar prato']
-    for tarefa in casa:
+    #casa.procurar('Lavar prato').concluir()
+    for tarefa in casa.tarefas:
+        print(f'- {tarefa}')
+    print(casa)
+
+    mercado = Projeto('Compras no mercado')
+    mercado.add('Frutas secas')
+    mercado.add('Tomate')
+    print(mercado)
+
+    comprar_carne = mercado.procurar('Carne')
+    comprar_carne.concluir()
+    for tarefa in mercado.tarefas:
         print(f'- {tarefa}')
 
 
