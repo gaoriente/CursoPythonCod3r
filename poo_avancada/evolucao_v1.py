ba0153cdb8d8fa06c9f8955437dec97cdfb9b597
Gabriel Oriente <gabriel.oriente@hotmail.com>
2021-07-02 17:46:15 -0300

    Update evolucao_v1.py

--- poo_avancada/evolucao_v1.py
+++ poo_avancada/evolucao_v1.py
@@ -4,6 +4,17 @@ class Humano:
 
     def __init__(self, nome):
         self.nome = nome
+        self._idade = None
+
+    # Get ler dado
+    def get_idade(self):
+        return self._idade
+
+    # Set alterar o dado
+    def set_idade(self, idade):
+        if idade < 0:
+            raise ValueError('Idade deve ser um número positivo!')
+        self._idade = idade
 
     def das_cavernas(self):
         self.especie = 'Homo Neanderthalensis'
@@ -29,10 +40,14 @@ class HomoSapiens(Humano):
 
 if __name__ == '__main__':
     jose = HomoSapiens('José')
-    grokn = Neanderthal('Grokn')
-    print(f'Evolução (a partir da classe): {",".join(HomoSapiens.especie())}')
-    print(f'Evolução (a partir da classe): {",".join(jose.especie())}')
-    print(f'Homo Sapiens evoluido? {HomoSapiens.is_evoluido()}')
-    print(f'Neanderthal evoluido? {Neanderthal.is_evoluido()}')
-    print(f'José evoluido? {jose.is_evoluido()}')
-    print(f'Grokn evoluido? {grokn.is_evoluido()}')
+    jose.set_idade(40)
+    print(f'Nome: {jose.nome} Idade: {jose.get_idade()}')
+
+    # jose = HomoSapiens('José')
+    # grokn = Neanderthal('Grokn')
+    # print(f'Evolução (a partir da classe): {",".join(HomoSapiens.especie())}')
+    # print(f'Evolução (a partir da classe): {",".join(jose.especie())}')
+    # print(f'Homo Sapiens evoluido? {HomoSapiens.is_evoluido()}')
+    # print(f'Neanderthal evoluido? {Neanderthal.is_evoluido()}')
+    # print(f'José evoluido? {jose.is_evoluido()}')
+    # print(f'Grokn evoluido? {grokn.is_evoluido()}')
