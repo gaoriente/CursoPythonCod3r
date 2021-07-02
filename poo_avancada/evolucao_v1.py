69a429ca6404bc5a0486926e48c3f4f76cfe68df
Gabriel Oriente <gabriel.oriente@hotmail.com>
2021-07-01 21:00:02 -0300

    Update evolucao_v1.py

--- poo_avancada/evolucao_v1.py
+++ poo_avancada/evolucao_v1.py
@@ -9,11 +9,30 @@ class Humano:
         self.especie = 'Homo Neanderthalensis'
         return self
 
+    @staticmethod
+    def especies():
+        adjetivos = ('Habilis', 'Erectus', 'Neanderthalensis', 'Sapiens')
+        return ('Aistralopiteco',) + tuple(f'Homo {adj}' for adj in adjetivos)
+
+    @classmethod
+    def is_evoluido(cls):
+        return cls.especie == cls.especies()[-1]
+
+
+class Neanderthal(Humano):
+    especie = Humano.especie()[-2]
+
+
+class HomoSapiens(Humano):
+    especie = Humano.especie()[-1]
+
 
 if __name__ == '__main__':
-    jose = Humano('José')
-    grokn = Humano('Grokn').das_cavernas()
-
-    print(f'Humano.especie: {Humano.especie}')
-    print(f'jose.especie: {jose.especie}')
-    print(f'grokn.especie: {grokn.especie}')
+    jose = HomoSapiens('José')
+    grokn = Neanderthal('Grokn')
+    print(f'Evolução (a partir da classe): {",".join(HomoSapiens.especie())}')
+    print(f'Evolução (a partir da classe): {",".join(jose.especie())}')
+    print(f'Homo Sapiens evoluido? {HomoSapiens.is_evoluido()}')
+    print(f'Neanderthal evoluido? {Neanderthal.is_evoluido()}')
+    print(f'José evoluido? {jose.is_evoluido()}')
+    print(f'Grokn evoluido? {grokn.is_evoluido()}')
